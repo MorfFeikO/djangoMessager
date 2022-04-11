811d7da9ccc1ef383094a3a301ca37011debee6e
Oleksandr Bozbei <oleksandr.bozbei@dxc.com>
2022-04-11 17:30:02 +0300

    add Message model

--- messager/models.py
+++ messager/models.py
@@ -13,3 +13,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Message(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name="message",
+        on_delete=models.DO_NOTHING,
+    )
+    body = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message -> {self.user.username}-{self.created_at}"
