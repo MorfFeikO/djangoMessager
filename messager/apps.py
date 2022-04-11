80dfc88ae3a7decac60e55e5778ee03cbc47ca63
Oleksandr Bozbei <oleksandr.bozbei@dxc.com>
2022-04-11 15:59:49 +0300

    create profile after user automaticaly

--- messager/apps.py
+++ messager/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MessagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'messager'
+
+    def ready(self):
+        import messager.signals # noqa
