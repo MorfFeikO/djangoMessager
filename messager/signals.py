d5af47198d5a59aae6f91062d03e8c569b2dfb72
Oleksandr Bozbei <oleksandr.bozbei@dxc.com>
2022-04-11 16:41:23 +0300

    refactor code

--- messager/signals.py
+++ messager/signals.py
@@ -8,3 +8,5 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
         user_profile.save()
+        user_profile.follows.add(instance.profile)
+        user_profile.save()
