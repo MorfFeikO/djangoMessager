4f4e6bf071e1093ea676d00ef27997da8f927c1f
Oleksandr Bozbei <oleksandr.bozbei@dxc.com>
2022-04-12 15:57:57 +0300

    add validations to registartion

--- djangomessager/urls.py
+++ djangomessager/urls.py
@@ -24,7 +24,7 @@ router.register(r'users', views.UserViewSet)
 router.register(r'messages', views.MessageViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path(
         'api-auth/',
         include('rest_framework.urls', namespace='rest_framework')
--- registration/serializers.py
+++ registration/serializers.py
@@ -1,18 +1,38 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 
 from .models import User
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())],
+    )
+    username = serializers.CharField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())],
+    )
     password = serializers.CharField(
-        max_length=256,
-        min_length=8,
-        write_only=True
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+    )
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
     )
 
     class Meta:
         model = User
-        fields = ['email', 'username', 'password']
+        fields = ['email', 'username', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
